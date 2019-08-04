9a38da88a9fb6242675c86b37a5cdc8cfdfcc245
Jorge Gallardo <JorgeGallardoG4@GMAIL.COM>
2019-08-04 16:49:05 -0500

    Compras

--- get/compras.php
+++ get/compras.php
@@ -1,24 +1,25 @@
 <?php
-$Fechain = "2019-04-01";
-$FechaFin = "2019-04-30";
+$Fechain = $_POST['finicial'];
+$FechaFin = $_POST['ffinal'];
+$bases = $_POST['bases'];
 include('../config/servicio.php');
-print_r($_POST);
 for ($i=0; $i<count($bases);$i++) {
 $conn=ibase_connect($servicedini.":".$rutadini.$empresadini[0]."",$usuariodini, $basedecode);
-$queryS ="SELECT PROVEEDOR_ID FROM PROVEEDORES";
+$queryS ="SELECT PROVEEDOR_ID, NOMBRE FROM PROVEEDORES";
 $Query=ibase_query($conn,$queryS);
 $cont=0;
 $i=0;
 while ($RowQ = ibase_fetch_object ($Query)) 
 {   
-    $queryCompras ="SELECT IMPORTE_NETO, TOTAL_IMPUESTOS, DSCTO_IMPORTE FROM DOCTOS_CM WHERE FECHA >= '2019-04-01' AND FECHA <= '2019-04-27' AND PROVEEDOR_ID=$RowQ->PROVEEDOR_ID ;";
+    $queryCompras ="SELECT IMPORTE_NETO, TOTAL_IMPUESTOS, DSCTO_IMPORTE FROM DOCTOS_CM WHERE FECHA >= '$Fechain' AND FECHA <= 'FechaFin' AND PROVEEDOR_ID=$RowQ->PROVEEDOR_ID ;";
     $QueryCompra=ibase_query($conn,$queryCompras);
    
     while ($RowQCompra = ibase_fetch_object ($QueryCompra)){
         $cont += $RowQCompra->IMPORTE_NETO+$RowQCompra->TOTAL_IMPUESTOS;
         $i++;
          }
+         echo $RowQ->NOMBRE." ".number_format($cont,2); 
 }
-echo number_format($cont,2); 
+
 }
 ?>
