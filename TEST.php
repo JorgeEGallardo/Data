5d694a365e2a0d78e3e2da18f0e6a4b26edb9da9
Jorge Gallardo <JorgeGallardoG4@GMAIL.COM>
2019-08-24 12:20:06 -0500

    s

--- TEST.php
+++ TEST.php
@@ -1,22 +1,40 @@
-<html>
-    <body>
+
 <?php
+header('Content-Type:text/csv; charset=latin1');
+header('Content-Disposition: attachment; filename="Reporte' . date("Y-m-d") . '.csv"');
+$salida = fopen('php://output', 'w');
+fputcsv($salida, array("Articulo","Descripcion","Existencia","Fecha compra","Costo compra","Fecha Venta"));
  include('config/servicio2.php');
  $conn = ibase_connect($servicedini . ":" . $rutadini . $empresadini[0] . "", $usuariodini, $basedecode);
  $QueryArticulos = "SELECT DISTINCT ARTICULO_ID, NOMBRE FROM ARTICULOS";
  $Articulo = ibase_query($conn, $QueryArticulos);
  while ($RowQArticulo = ibase_fetch_object($Articulo)) {
-     /*echo "<br>";
+    $Ex="";
+    $QueryExis ="select * from EXIVAL_ART($RowQArticulo->ARTICULO_ID,0 ,'2019-11-12','S');";
+    $Exis= ibase_query($conn, $QueryExis);
+    while ($RowqExis = ibase_fetch_object($Exis)) {
+        $Ex = $RowqExis->EXISTENCIA;
+        }
+    $QueryULT = "SELECT A.DOCTO_VE_ID,A.ARTICULO_ID,B.FECHA
+    FROM DOCTOS_VE_DET A 
+    JOIN DOCTOS_VE B ON(A.DOCTO_VE_ID=B.DOCTO_VE_ID)
+    WHERE ARTICULO_ID = $RowQArticulo->ARTICULO_ID
+    AND ROL <> 'C' ORDER BY B.FECHA;";
+    $ULT = ibase_query($conn, $QueryULT);
+    $fecha="";
+    while ($RowQULT = ibase_fetch_object($ULT)) {
+    $fecha = $RowQULT->FECHA;
+    }
     $QueryULT = "SELECT * FROM GET_ULTCOM_ART($RowQArticulo->ARTICULO_ID)";
     $ULT = ibase_query($conn, $QueryULT);
     while ($RowQULT = ibase_fetch_object($ULT)) {
-    //echo "<br>".$RowQArticulo->NOMBRE."<br>".$RowQULT->FECHA_ULTIMA_COMPRA."<br>".$RowQULT->COSTO_ULTIMA_COMPRA."<br>";
-    }*/
-   $QueryULT = "SELECT * FROM GET_FECHA_ULTVEN_PV($RowQArticulo->ARTICULO_ID,0,'2019-08-19','S')";
-   $ULT = ibase_query($conn, $QueryULT);
-   while ($RowQULT = ibase_fetch_object($ULT)) {
-   echo "<br>".$RowQULT->FECHA."<br>";
+        fputcsv($salida, array($RowQArticulo->ARTICULO_ID,$RowQArticulo->NOMBRE,$Ex,$RowQULT->FECHA_ULTIMA_COMPRA,$RowQULT->COSTO_ULTIMA_COMPRA,$fecha));
+    }
+    /*
+    SELECT A.DOCTO_VE_ID,A.ARTICULO_ID,B.FECHA
+    FROM DOCTOS_VE_DET A 
+    JOIN DOCTOS_VE B ON(A.DOCTO_VE_ID=B.DOCTO_VE_ID)
+    WHERE ARTICULO_ID = 12194601
+    AND ROL <> 'C' ORDER BY B.FECHA;*/
    }
-   echo "<br>";
-}
  
