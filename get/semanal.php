a3731c633586cca43e40e71d476c361f7401fc73
Jorge Gallardo <JorgeGallardoG4@GMAIL.COM>
2019-09-07 12:04:33 -0500

    Actualizaci¾n automatica

--- get/semanal.php
+++ get/semanal.php
@@ -1,26 +1,17 @@
 <?php
-$jsons = array("{label: 'Compras',
-    borderColor: 'green',
-    backgroundColor: 'orange',
-    fill: false,
-    data: [
-        1,2,3,4,5,6,7
-    ],
-    yAxisID: 'y-axis-1'
-},", "{
-    label: 'Recuperacion,
-    fill: false,
-    borderColor: 'green',
-    backgroundColor: 'orange',
-    data: [8,2,1,2,3,4,5,6,7
-    ],
-    yAxisID: 'y-axis-2'
-},");
-
-
+$jsons = array(
+    "{label: 'Inventarios', borderColor: 'green',  backgroundColor: 'orange',  fill: false,  data: [ 1,2,3,4,5,6,7 ],yAxisID: 'y-axis-1'}",
+    "{label: 'Ventas',    borderColor: 'green',    backgroundColor: 'orange',    fill: false,    data: [        2,9,3,4,2,6,7    ],    yAxisID: 'y-axis-1'}",
+    "{label: 'CXC',    borderColor: 'green',    backgroundColor: 'orange',    fill: false,    data: [        3,9,3,4,2,6,7    ],    yAxisID: 'y-axis-1'}",
+    "{label: 'Recuperacion',    borderColor: 'green',    backgroundColor: 'orange',    fill: false,    data: [        4,9,3,4,2,6,7    ],    yAxisID: 'y-axis-1'}",
+    "{label: 'Compras',    borderColor: 'green',    backgroundColor: 'orange',    fill: false,    data: [        5,9,3,4,2,6,7    ],    yAxisID: 'y-axis-1'}",
+    "{label: 'Proveedores',    borderColor: 'green',    backgroundColor: 'orange',    fill: false,    data: [        6,9,3,4,2,6,7    ],    yAxisID: 'y-axis-1'}"
+);
+$string = "";
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
@@ -30,13 +21,13 @@ $jsons = array("{label: 'Compras',
 <div class="col-md-12">
     <div class="card">
         <div class="card-body">
-            <h5 class="card-title m-b-0"><?php echo str_replace(".FDB", "", $empresadini[$bd]); ?></h5>
+            <h5 class="card-title m-b-0">Empresa</h5>
         </div>
         <table class="table">
             <thead>
                 <tr>
-                    
-                <th scope="col">Grafica</th>
+
+                    <th scope="col">Grafica</th>
                     <th scope="col">Elemento</th>
                     <th scope="col">Valor</th>
                     <th scope="col">Fecha</th>
@@ -44,47 +35,47 @@ $jsons = array("{label: 'Compras',
             </thead>
             <tbody>
                 <form>
-                <tr>
-                    <td><input type="checkbox" ></input></td>
-                    <td>Inventario</td>
-                    <td id="m1"><?php echo number_format(123.2, 2); ?></td>
-                    <td><?php echo $ExivalF ?></td>
-                </tr>
-                <tr>
-                <td><input type="checkbox" onclick='F()'></input></td>
-                    <td>Ventas a clientes</td>
-                    <td id="m2"><?php echo number_format(123.2, 2); ?></td>
-                    <td><?php echo $VentasF ?></td>
-                </tr>
-                <tr>
-                <td><input type="checkbox"></input></td>
-                    <td>Cuentas por cobrar</td>
-                    <td id="m3"><?php echo number_format(123.2, 2); ?></td>
-                    <td><?php echo $CXCF ?></td>
-                </tr>
+                    <tr>
+                        <td><input type="checkbox" id="1" onclick='F();'></input></td>
+                        <td>Inventario</td>
+                        <td id="m1"><?php echo number_format(123.2, 2); ?></td>
+                        <td><?php echo $ExivalF ?></td>
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox" id="2" onclick='F()'></input></td>
+                        <td>Ventas a clientes</td>
+                        <td id="m2"><?php echo number_format(123.2, 2); ?></td>
+                        <td><?php echo $VentasF ?></td>
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox" id="3" onclick='F()'></input></td>
+                        <td>Cuentas por cobrar</td>
+                        <td id="m3"><?php echo number_format(123.2, 2); ?></td>
+                        <td><?php echo $CXCF ?></td>
+                    </tr>
 
-                <tr>
-                <td><input type="checkbox"></input></td>
-                    <td>Recuperacion de cartera</td>
-                    <td id="m5"><?php echo number_format(123.2, 2); ?> </td>
-                    <td><?php echo $RecF ?></td>
-                </tr>
-                <tr>
-                <td><input type="checkbox"></input></td>
-                    <td>Compras</td>
-                    <td id="m4"><?php echo number_format(123.2, 2); ?></td>
-                    <td><?php echo $ComprasF ?></td>
-                </tr>
-                <tr>
-                <td><input type="checkbox"></input></td>
-                    <td>Proveedores</td>
-                    <td id="m4"><?php echo number_format(123.2, 2); ?></td>
-                    <td><?php echo $ProvF ?></td>
-                </tr>
-</form>
+                    <tr>
+                        <td><input type="checkbox" id="4" onclick='F()'></input></td>
+                        <td>Recuperacion de cartera</td>
+                        <td id="m5"><?php echo number_format(123.2, 2); ?> </td>
+                        <td><?php echo $RecF ?></td>
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox" id="5" onclick='F()'></input></td>
+                        <td>Compras</td>
+                        <td id="m4"><?php echo number_format(123.2, 2); ?></td>
+                        <td><?php echo $ComprasF ?></td>
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox" id="6" onclick='F()'></input></td>
+                        <td>Proveedores</td>
+                        <td id="m4"><?php echo number_format(123.2, 2); ?></td>
+                        <td><?php echo $ProvF ?></td>
+                    </tr>
+                </form>
             </tbody>
         </table>
-        
+
     </div>
 
     <div class="card">
@@ -102,7 +93,7 @@ $jsons = array("{label: 'Compras',
                 datasets: [
                     <?php
                     echo $jsons[0];
-                    ?> {
+                    ?> ,{
                         label: 'Ventas',
                         borderColor: "green",
                         backgroundColor: "red",
@@ -114,30 +105,86 @@ $jsons = array("{label: 'Compras',
                     }
                 ]
             };
-function F(){
-    var lineChartData = {
-                labels: ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'],
-                datasets: [
-                    <?php
-                    echo $jsons[0];
-                    ?> {
-                        label: 'Ventas',
-                        borderColor: "green",
-                        backgroundColor: "red",
-                        fill: false,
-                        data: [
-                            9, 9, 3, 4, 5, 6, 7
-                        ],
-                        yAxisID: 'y-axis-1',
-                    }
-                ]
-            };
-   noF();
-}
-function noF(){
-    var ctx = document.getElementById('canvas').getContext('2d');
+
+            function F1() {
+                if ($("#1").is(':checked')) {
+                    <?php $string =  $string . $jsons[0]; ?>;
+                }
+            }
+
+            function F2() {
+                if ($("#2").is(':checked')) {
+                    <?php $string =  $string . $jsons[1]; ?>;
+                }
+            }
+
+            function F3() {
+                if ($("#3").is(':checked')) {
+                    <?php $string =  $string . $jsons[2]; ?>;
+                }
+            }
+
+            function F4() {
+                if ($("#4").is(':checked')) {
+                    <?php $string =  $string . $jsons[3]; ?>;
+                }
+            }
+
+            function F5() {
+                if ($("#5").is(':checked')) {
+                    <?php $string =  $string . $jsons[4]; ?>;
+                }
+            }
+
+            function F6() {
+                if ($("#6").is(':checked')) {
+                    alert("wot?");
+                    <?php $string = $string . $jsons[5]; ?>;
+                }
+            }
+
+
+            function F() {
+               
+                var dataset1 = {};
+                var dataset2 = {};
+                var dataset3 = {};
+                var dataset4 = {};
+                var dataset5 = {};
+                var dataset6 = {};
+                    if ($("#1").is(':checked')) {
+                        dataset1 = <?php echo $jsons[0]; ?>;
+                }
+                if ($("#2").is(':checked')) {
+                        dataset2 = <?php echo $jsons[1]; ?>;
+                }
+                if ($("#3").is(':checked')) {
+                        dataset3 = <?php echo $jsons[2]; ?>;
+                }
+                if ($("#4").is(':checked')) {
+                        dataset4 = <?php echo $jsons[3]; ?>;
+                }
+                if ($("#5").is(':checked')) {
+                        dataset5 = <?php echo $jsons[4]; ?>;
+                }
+                if ($("#6").is(':checked')) {
+                        dataset6 = <?php echo $jsons[5]; ?>;
+                }
+
+                var lineChartData2 = {
+                    labels: ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'],
+                    datasets: [
+                        dataset1,
+                        dataset2,
+                        dataset3,
+                        dataset4,
+                        dataset5,
+                        dataset6
+                    ]
+                };
+                var ctx = document.getElementById('canvas').getContext('2d');
                 window.myLine = Chart.Line(ctx, {
-                    data: lineChartData,
+                    data: lineChartData2,
                     options: {
                         responsive: true,
                         hoverMode: 'index',
@@ -166,7 +213,7 @@ function noF(){
                         }
                     }
                 });
-}
+            }
             window.onload = function() {
                 var ctx = document.getElementById('canvas').getContext('2d');
                 window.myLine = Chart.Line(ctx, {
