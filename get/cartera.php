ecf3ae24305a005ea08284e01878c03efaa7c380
Data <jorgegallardog4@gmail.com>
2019-09-25 14:04:58 -0500

    v2

--- get/cartera.php
+++ get/cartera.php
@@ -1,60 +1,59 @@
 <?php
-
-if (1 == 2) {
-    echo "Error1";
-    exit;
-} else {
-    $WhereTemp = "";
-    $WhereClientes = "";
-    $Where = "";
-    if ($_POST["estatus"] != "") {
-        $WhereClientes = $WhereClientes . " AND ESTATUS= '" . $_POST["estatus"] . "'";
-    }
-    if ($_POST["Pago"] != "") {
-        $WhereClientes = $WhereClientes . " AND COND_PAGO_ID= '" . $_POST["Pago"] . "'";
-    }
-    if ($_POST["vend"] != "") {
-        $WhereClientes = $WhereClientes . " AND VENDEDOR_ID= '" . $_POST["vend"] . "'";
-    }
-    if ($_POST["zona"] != "") {
-        $WhereClientes = $WhereClientes . " AND ZONA_CLIENTE_ID= " . $_POST["zona"] . "";
-    }
-    if ($WhereTemp != "") {
-        $Where = "WHERE 1=1 " . $WhereTemp;
-    }
-    $empresas = array();
-    $total = array();
-    include('../config/servicio.php');
-    $bases = $_POST["bases"];
-    $in = false;
-    $headerset = true;
-    for ($i = 0; $i < count($bases); $i++) {
-        $conn = ibase_connect($servicedini . ":" . $rutadini . $empresadini[$bases[$i]] . "", $usuariodini, $basedecode);
-        if (!$conn) {
-            echo "Error2";
-            exit;
-        } else {
-            $temp = $bases[$i];
-            $limp = str_replace(".FDB", "", $empresadini[$temp]);
-            if ($headerset == true) {
-                if (count($bases) == 1) {
-                    header('Content-Type:text/csv; charset=latin1');
-                    header('Content-Disposition: attachment; filename="Reporte ' . $limp . ' ' . date("Y-m-d") . '.csv"');
-                } else {
-                    header('Content-Type:text/csv; charset=latin1');
-                    header('Content-Disposition: attachment; filename="Reporte grupal ' . date("Y-m-d") . '.csv"');
-                }
-                $headerset = false;
+$arrayLines = array();
+$WhereTemp = "";
+$WhereClientes = "";
+$Where = "";
+if ($_POST["estatus"] != "") {
+    $WhereClientes = $WhereClientes . " AND ESTATUS= '" . $_POST["estatus"] . "'";
+}
+if ($_POST["Pago"] != "") {
+    $WhereClientes = $WhereClientes . " AND COND_PAGO_ID= '" . $_POST["Pago"] . "'";
+}
+if ($_POST["vend"] != "") {
+    $WhereClientes = $WhereClientes . " AND VENDEDOR_ID= '" . $_POST["vend"] . "'";
+}
+if ($_POST["zona"] != "") {
+    $WhereClientes = $WhereClientes . " AND ZONA_CLIENTE_ID= " . $_POST["zona"] . "";
+}
+if ($WhereTemp != "") {
+    $Where = "WHERE 1=1 " . $WhereTemp;
+}
+$empresas = array();
+$total = array();
+include('../config/servicio.php');
+$bases = $_POST["bases"];
+$in = false;
+$headerset = true;
+for ($i = 0; $i < count($bases); $i++) {
+    $conn = ibase_connect($servicedini . ":" . $rutadini . $empresadini[$bases[$i]] . "", $usuariodini, $basedecode);
+    if (!$conn) {
+        echo "Error2";
+        exit;
+    } else {
+        $temp = $bases[$i];
+        $limp = str_replace(".FDB", "", $empresadini[$temp]);
+        if ($headerset == true) {
+            if (count($bases) == 1) {
+                header('Content-Type:text/csv; charset=latin1');
+                header('Content-Disposition: attachment; filename="Reporte ' . $limp . ' ' . date("Y-m-d") . '.csv"');
+            } else {
+                header('Content-Type:text/csv; charset=latin1');
+                header('Content-Disposition: attachment; filename="Reporte grupal ' . date("Y-m-d") . '.csv"');
             }
-            $salida = fopen('php://output', 'w');
+            $headerset = false;
+        }
+        $salida = fopen('php://output', 'w');
 
-            fputcsv($salida, array("Reporte: " . $limp));
-            fputcsv($salida, array("Clave cliente", "Nombre", "Saldo", "Por vencer", "Por vencer 0 a 30", "Por vencer 31 a 60", "Por vencer mas de 60", "Vencido", "Vencido de 0 a 30", "Vencido de 31 a 60", "Vencido mas de 60", "Estatus", "Zona", "Forma de pago", "Tipo", "Vendedor", "Limite de credito"));
+        fputcsv($salida, array("Reporte: " . $limp));
 
 
-                                //Fecha parametro
-                                $fecha_ini = $_POST['finicial'] ;
-                                $fecha_fin = $_POST['ffinal'] ;
+        //Fecha parametro
+        for ($l = 0; $l < 4; $l++) {
+            $month = date("m");
+            $month -=$l;
+            $month = "0".$month;
+            $fecha_fin = date("Y")."-".$month.date("-t"); 
+            $fecha_ini = date("Y")."-".$month."-01";
             $tot = 0;
             $tot30 = 0;
             $tot60 = 0;
@@ -66,7 +65,7 @@ if (1 == 2) {
                 echo $QueryClientes;
             } else {
                 while ($RowQCliente = ibase_fetch_object($Cliente)) {
-                    $s=0;
+                    $s = 0;
                     $QueryClienteInfo = "SELECT A.SALDO_VENCIDO, A.SALDO_X_VENCER, A.SALDO_CXC, B.LIMITE_CREDITO,B.ESTATUS,A.CLIENTE_ID,A.SALDO_VENCIDO_PER1,A.SALDO_VENCIDO_PER2, A.SALDO_VENCIDO_PER3, B.Nombre, C.Nombre, A.SALDO_X_VENCER_PER1, A.SALDO_X_VENCER_PER2, A.SALDO_X_VENCER_PER3,A.SALDO_X_VENCER, D.CLAVE_CLIENTE, E.NOMBRE, F.NOMBRE, G.NOMBRE
                     FROM ORSP_CL_ANTSAL_CLI_EX($RowQCliente->CLIENTE_ID, '" . $fecha_ini . "', '" . $fecha_fin . "', 30, 'N', 'S') A
                     INNER JOIN Clientes B ON(A.CLIENTE_ID=B.CLIENTE_ID)
@@ -94,9 +93,8 @@ if (1 == 2) {
                         } else if ($RowVencimiento->ESTATUS == "V") {
                             $ESTATUS = "Suspendido";
                         }
-                        fputcsv($salida, array($RowVencimiento->CLAVE_CLIENTE, $RowVencimiento->NOMBRE,  $RowVencimiento->SALDO_CXC, $RowVencimiento->SALDO_X_VENCER, $RowVencimiento->SALDO_X_VENCER_PER1, $RowVencimiento->SALDO_X_VENCER_PER2, $RowVencimiento->SALDO_X_VENCER_PER3, $RowVencimiento->SALDO_VENCIDO, $RowVencimiento->SALDO_VENCIDO_PER1, $RowVencimiento->SALDO_VENCIDO_PER2, $RowVencimiento->SALDO_VENCIDO_PER3, $ESTATUS, $RowVencimiento->NOMBRE_01, $RowVencimiento->NOMBRE_02, $RowVencimiento->NOMBRE_03, $RowVencimiento->NOMBRE_04, $RowVencimiento->LIMITE_CREDITO));
                     }
-                    if($s==0){
+                    if ($s == 0) {
                         $QueryClienteInfo = "SELECT A.SALDO_VENCIDO, A.SALDO_X_VENCER, A.SALDO_CXC, B.LIMITE_CREDITO,B.ESTATUS,A.CLIENTE_ID,A.SALDO_VENCIDO_PER1,A.SALDO_VENCIDO_PER2, A.SALDO_VENCIDO_PER3, B.Nombre, A.SALDO_X_VENCER_PER1, A.SALDO_X_VENCER_PER2, A.SALDO_X_VENCER_PER3,A.SALDO_X_VENCER, D.CLAVE_CLIENTE, E.NOMBRE
                         FROM ORSP_CL_ANTSAL_CLI_EX($RowQCliente->CLIENTE_ID, '" . $fecha_ini . "', '" . $fecha_fin . "', 30, 'N', 'S') A
                         INNER JOIN Clientes B ON(A.CLIENTE_ID=B.CLIENTE_ID)
@@ -106,7 +104,7 @@ if (1 == 2) {
                         if (!$Vencidos) {
                             echo "No se puede mostrar la consulta vencimientos: " . $QueryCredito . "<br/>";
                         }
-    
+
                         while ($RowVencimiento = ibase_fetch_object($Vencidos)) {
                             $SALDO_VENCIDO_PER1 = $RowVencimiento->SALDO_VENCIDO_PER1;
                             $SALDO_VENCIDO_PER2 = $RowVencimiento->SALDO_VENCIDO_PER2;
@@ -120,17 +118,17 @@ if (1 == 2) {
                             } else if ($RowVencimiento->ESTATUS == "V") {
                                 $ESTATUS = "Suspendido";
                             }
-                            fputcsv($salida, array($RowVencimiento->CLAVE_CLIENTE, $RowVencimiento->NOMBRE,  $RowVencimiento->SALDO_CXC, $RowVencimiento->SALDO_X_VENCER, $RowVencimiento->SALDO_X_VENCER_PER1, $RowVencimiento->SALDO_X_VENCER_PER2, $RowVencimiento->SALDO_X_VENCER_PER3, $RowVencimiento->SALDO_VENCIDO, $RowVencimiento->SALDO_VENCIDO_PER1, $RowVencimiento->SALDO_VENCIDO_PER2, $RowVencimiento->SALDO_VENCIDO_PER3, $ESTATUS, "-",$RowVencimiento->NOMBRE_01,"-","-", $RowVencimiento->LIMITE_CREDITO));
+                            array_push($arrayLines, array($RowVencimiento->CLAVE_CLIENTE, $RowVencimiento->NOMBRE,  $RowVencimiento->SALDO_CXC, $RowVencimiento->SALDO_X_VENCER, $RowVencimiento->SALDO_X_VENCER_PER1, $RowVencimiento->SALDO_X_VENCER_PER2, $RowVencimiento->SALDO_X_VENCER_PER3, $RowVencimiento->SALDO_VENCIDO, $RowVencimiento->SALDO_VENCIDO_PER1, $RowVencimiento->SALDO_VENCIDO_PER2, $RowVencimiento->SALDO_VENCIDO_PER3, $ESTATUS, "-", $RowVencimiento->NOMBRE_01, "-", "-", $RowVencimiento->LIMITE_CREDITO));
                         }
                     }
                 }
             }
-            ?>
-
-                                                <?php
-                                                }
-                                            }
-                                        }
-
-
-                                        ?>
+        }
+        $output = array();
+        foreach ($arrayLines as $Line) {
+            array_push($output,"Clave cliente", "Nombre", "Saldo", "Por vencer", "Por vencer 0 a 30", "Por vencer 31 a 60", "Por vencer mas de 60", "Vencido", "Vencido de 0 a 30", "Vencido de 31 a 60", "Vencido mas de 60", "Estatus", "Zona", "Forma de pago", "Tipo", "Vendedor", "Limite de credito");
+        }
+                  fputcsv($salida,$output);
+        
+    }
+}
