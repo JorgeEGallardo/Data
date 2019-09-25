e1921d839ac4c35e177ea9bde6cfff4b928d437a
Data <jorgegallardog4@gmail.com>
2019-09-25 15:01:53 -0500

    v2

--- get/cartera.php
+++ get/cartera.php
@@ -48,24 +48,23 @@ for ($i = 0; $i < count($bases); $i++) {
 
 
         //Fecha parametro
-        for ($l = 0; $l < 2; $l++) {
-            $fecha_fin = date("y-m-d");
-            $fecha_ini = date('y-m-d', strtotime('-1 month'));
-            echo $fecha_fin;
-            echo $fecha_ini;
-            $tot = 0;
-            $tot30 = 0;
-            $tot60 = 0;
+        $fecha_fin = date("Y-m-d");
+        $fecha_ini = date('Y-m-d', strtotime('-5 month'));
+        echo $fecha_fin;
+        echo $fecha_ini;
+        $tot = 0;
+        $tot30 = 0;
+        $tot60 = 0;
 
 
-            $QueryClientes = "SELECT DISTINCT CLIENTE_ID FROM CLIENTES";
-            $Cliente = ibase_query($conn, $QueryClientes);
-            if (!$Cliente) {
-                echo $QueryClientes;
-            } else {
-                while ($RowQCliente = ibase_fetch_object($Cliente)) {
-                    $s = 0;
-                    $QueryClienteInfo = "SELECT A.SALDO_VENCIDO, A.SALDO_X_VENCER, A.SALDO_CXC, B.LIMITE_CREDITO,B.ESTATUS,A.CLIENTE_ID,A.SALDO_VENCIDO_PER1,A.SALDO_VENCIDO_PER2, A.SALDO_VENCIDO_PER3, B.Nombre, C.Nombre, A.SALDO_X_VENCER_PER1, A.SALDO_X_VENCER_PER2, A.SALDO_X_VENCER_PER3,A.SALDO_X_VENCER, D.CLAVE_CLIENTE, E.NOMBRE, F.NOMBRE, G.NOMBRE
+        $QueryClientes = "SELECT DISTINCT CLIENTE_ID FROM CLIENTES";
+        $Cliente = ibase_query($conn, $QueryClientes);
+        if (!$Cliente) {
+            echo $QueryClientes;
+        } else {
+            while ($RowQCliente = ibase_fetch_object($Cliente)) {
+                $s = 0;
+                $QueryClienteInfo = "SELECT A.SALDO_VENCIDO, A.SALDO_X_VENCER, A.SALDO_CXC, B.LIMITE_CREDITO,B.ESTATUS,A.CLIENTE_ID,A.SALDO_VENCIDO_PER1,A.SALDO_VENCIDO_PER2, A.SALDO_VENCIDO_PER3, B.Nombre, C.Nombre, A.SALDO_X_VENCER_PER1, A.SALDO_X_VENCER_PER2, A.SALDO_X_VENCER_PER3,A.SALDO_X_VENCER, D.CLAVE_CLIENTE, E.NOMBRE, F.NOMBRE, G.NOMBRE
                     FROM ORSP_CL_ANTSAL_CLI_EX($RowQCliente->CLIENTE_ID, '" . $fecha_ini . "', '" . $fecha_fin . "', 30, 'N', 'S') A
                     INNER JOIN Clientes B ON(A.CLIENTE_ID=B.CLIENTE_ID)
                     INNER JOIN ZONAS_CLIENTES C ON(B.Zona_Cliente_ID = C.zona_Cliente_ID) 
@@ -73,13 +72,38 @@ for ($i = 0; $i < count($bases); $i++) {
                     INNER JOIN CONDICIONES_PAGO E ON (B.COND_PAGO_ID = E.COND_PAGO_ID)
                     INNER JOIN TIPOS_CLIENTES F ON(F.TIPO_CLIENTE_ID=B.TIPO_CLIENTE_ID)
                     INNER JOIN VENDEDORES G ON(B.VENDEDOR_ID = G.VENDEDOR_ID) $Where;";
+                $Vencidos = ibase_query($conn, $QueryClienteInfo);
+                if (!$Vencidos) {
+                    echo "No se puede mostrar la consulta vencimientos: " . $QueryCredito . "<br/>";
+                }
+
+                while ($RowVencimiento = ibase_fetch_object($Vencidos)) {
+                    $s++;
+                    $SALDO_VENCIDO_PER1 = $RowVencimiento->SALDO_VENCIDO_PER1;
+                    $SALDO_VENCIDO_PER2 = $RowVencimiento->SALDO_VENCIDO_PER2;
+                    $SALDO_VENCIDO_PER3 = $RowVencimiento->SALDO_VENCIDO_PER3;
+                    if ($RowVencimiento->ESTATUS == "A") {
+                        $ESTATUS = "Activo";
+                    } else if ($RowVencimiento->ESTATUS == "B") {
+                        $ESTATUS = "Inactivo";
+                    } else if ($RowVencimiento->ESTATUS == "C") {
+                        $ESTATUS = "Suspendido";
+                    } else if ($RowVencimiento->ESTATUS == "V") {
+                        $ESTATUS = "Suspendido";
+                    }
+                }
+                if ($s == 0) {
+                    $QueryClienteInfo = "SELECT A.SALDO_VENCIDO, A.SALDO_X_VENCER, A.SALDO_CXC, B.LIMITE_CREDITO,B.ESTATUS,A.CLIENTE_ID,A.SALDO_VENCIDO_PER1,A.SALDO_VENCIDO_PER2, A.SALDO_VENCIDO_PER3, B.Nombre, A.SALDO_X_VENCER_PER1, A.SALDO_X_VENCER_PER2, A.SALDO_X_VENCER_PER3,A.SALDO_X_VENCER, D.CLAVE_CLIENTE, E.NOMBRE
+                        FROM ORSP_CL_ANTSAL_CLI_EX($RowQCliente->CLIENTE_ID, '" . $fecha_ini . "', '" . $fecha_fin . "', 30, 'N', 'S') A
+                        INNER JOIN Clientes B ON(A.CLIENTE_ID=B.CLIENTE_ID)
+                        INNER JOIN CLAVES_CLIENTES D ON(D.CLIENTE_ID = B.CLIENTE_ID)
+                        INNER JOIN CONDICIONES_PAGO E ON (B.COND_PAGO_ID = E.COND_PAGO_ID) $Where;";
                     $Vencidos = ibase_query($conn, $QueryClienteInfo);
                     if (!$Vencidos) {
                         echo "No se puede mostrar la consulta vencimientos: " . $QueryCredito . "<br/>";
                     }
 
                     while ($RowVencimiento = ibase_fetch_object($Vencidos)) {
-                        $s++;
                         $SALDO_VENCIDO_PER1 = $RowVencimiento->SALDO_VENCIDO_PER1;
                         $SALDO_VENCIDO_PER2 = $RowVencimiento->SALDO_VENCIDO_PER2;
                         $SALDO_VENCIDO_PER3 = $RowVencimiento->SALDO_VENCIDO_PER3;
@@ -92,42 +116,10 @@ for ($i = 0; $i < count($bases); $i++) {
                         } else if ($RowVencimiento->ESTATUS == "V") {
                             $ESTATUS = "Suspendido";
                         }
-                    }
-                    if ($s == 0) {
-                        $QueryClienteInfo = "SELECT A.SALDO_VENCIDO, A.SALDO_X_VENCER, A.SALDO_CXC, B.LIMITE_CREDITO,B.ESTATUS,A.CLIENTE_ID,A.SALDO_VENCIDO_PER1,A.SALDO_VENCIDO_PER2, A.SALDO_VENCIDO_PER3, B.Nombre, A.SALDO_X_VENCER_PER1, A.SALDO_X_VENCER_PER2, A.SALDO_X_VENCER_PER3,A.SALDO_X_VENCER, D.CLAVE_CLIENTE, E.NOMBRE
-                        FROM ORSP_CL_ANTSAL_CLI_EX($RowQCliente->CLIENTE_ID, '" . $fecha_ini . "', '" . $fecha_fin . "', 30, 'N', 'S') A
-                        INNER JOIN Clientes B ON(A.CLIENTE_ID=B.CLIENTE_ID)
-                        INNER JOIN CLAVES_CLIENTES D ON(D.CLIENTE_ID = B.CLIENTE_ID)
-                        INNER JOIN CONDICIONES_PAGO E ON (B.COND_PAGO_ID = E.COND_PAGO_ID) $Where;";
-                        $Vencidos = ibase_query($conn, $QueryClienteInfo);
-                        if (!$Vencidos) {
-                            echo "No se puede mostrar la consulta vencimientos: " . $QueryCredito . "<br/>";
-                        }
-
-                        while ($RowVencimiento = ibase_fetch_object($Vencidos)) {
-                            $SALDO_VENCIDO_PER1 = $RowVencimiento->SALDO_VENCIDO_PER1;
-                            $SALDO_VENCIDO_PER2 = $RowVencimiento->SALDO_VENCIDO_PER2;
-                            $SALDO_VENCIDO_PER3 = $RowVencimiento->SALDO_VENCIDO_PER3;
-                            if ($RowVencimiento->ESTATUS == "A") {
-                                $ESTATUS = "Activo";
-                            } else if ($RowVencimiento->ESTATUS == "B") {
-                                $ESTATUS = "Inactivo";
-                            } else if ($RowVencimiento->ESTATUS == "C") {
-                                $ESTATUS = "Suspendido";
-                            } else if ($RowVencimiento->ESTATUS == "V") {
-                                $ESTATUS = "Suspendido";
-                            }
-                            array_push($arrayLines, array($RowVencimiento->CLAVE_CLIENTE, $RowVencimiento->NOMBRE,  $RowVencimiento->SALDO_CXC, $RowVencimiento->SALDO_X_VENCER, $RowVencimiento->SALDO_X_VENCER_PER1, $RowVencimiento->SALDO_X_VENCER_PER2, $RowVencimiento->SALDO_X_VENCER_PER3, $RowVencimiento->SALDO_VENCIDO, $RowVencimiento->SALDO_VENCIDO_PER1, $RowVencimiento->SALDO_VENCIDO_PER2, $RowVencimiento->SALDO_VENCIDO_PER3, $ESTATUS, "-", $RowVencimiento->NOMBRE_01, "-", "-", $RowVencimiento->LIMITE_CREDITO));
-                        }
+                        fputcsv($salida, array($RowVencimiento->CLAVE_CLIENTE, $RowVencimiento->NOMBRE,  $RowVencimiento->SALDO_CXC, $RowVencimiento->SALDO_X_VENCER, $RowVencimiento->SALDO_X_VENCER_PER1, $RowVencimiento->SALDO_X_VENCER_PER2, $RowVencimiento->SALDO_X_VENCER_PER3, $RowVencimiento->SALDO_VENCIDO, $RowVencimiento->SALDO_VENCIDO_PER1, $RowVencimiento->SALDO_VENCIDO_PER2, $RowVencimiento->SALDO_VENCIDO_PER3, $ESTATUS, "-", $RowVencimiento->NOMBRE_01, "-", "-", $RowVencimiento->LIMITE_CREDITO));
                     }
                 }
             }
         }
-        $output = array();
-        foreach ($arrayLines as $Line) {
-            array_push($output,"Clave cliente", "Nombre", "Saldo", "Por vencer", "Por vencer 0 a 30", "Por vencer 31 a 60", "Por vencer mas de 60", "Vencido", "Vencido de 0 a 30", "Vencido de 31 a 60", "Vencido mas de 60", "Estatus", "Zona", "Forma de pago", "Tipo", "Vendedor", "Limite de credito");
-        }
-                  fputcsv($salida,$output);
-        
     }
 }
