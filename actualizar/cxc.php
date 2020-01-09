1c13824af449a87c863045367a3ffa27f75fca83
Data <jorgegallardog4@gmail.com>
2020-01-09 12:33:03 -0600

    awa

--- actualizar/cxc.php
+++ actualizar/cxc.php
@@ -1,5 +1,5 @@
 <?PHP
-
+$time_start = microtime(true); 
 $y = date("y");
 $Fechain = "20$y-01-01";
 $FechaFin = "20".date("y-m-d");
@@ -9,24 +9,35 @@ for ($i=0; $i < count($empresadini) ; $i++) {
 //-----------------------//
 //--------cxc-------//
 $conn=ibase_connect($servicedini.":".$rutadini.$empresadini[$i]."",$usuariodini, $basedecode);
-$QueryClientes="SELECT DISTINCT CLIENTE_ID 
-FROM CLIENTES";
+$QueryClientes="select sum(A.SALDO_CXC)
+from CLIENTES c
+LEFT join ORSP_CL_ANTSAL_CLI_EX(c.CLIENTE_ID, '$Fechain', '$FechaFin', 30, 'N', 'S') A on (1=1);";
 $Cliente = ibase_query($conn, $QueryClientes);
 $cont= 0 ; 
 if (!$Cliente) {
     echo $QueryClientes;
 }else {
 while ($RowQCliente = ibase_fetch_object($Cliente)) {
-    $QueryClienteInfo = "SELECT A.SALDO_CXC
-    FROM ORSP_CL_ANTSAL_CLI_EX($RowQCliente->CLIENTE_ID, '".$Fechain."', '".$FechaFin."', 30, 'N', 'S') A;";   
-$ClienteInfo = ibase_query($conn, $QueryClienteInfo);
-while ($RowQClienteInfo = ibase_fetch_object($ClienteInfo)) {
-    $cont+=$RowQClienteInfo->SALDO_CXC; 
+  $cont = $RowQCliente->SUM;
 }
 }
-}
+
 $conn2=ibase_connect($servicedini.":".$rutadini."DASHBOARD.FDB",$usuariodini, $basedecode);	
 $Query = "INSERT INTO CXC (VALOR, BD) VALUES ($cont,'$empresadini[$i]');";  
 $CXC= ibase_query($conn2, $Query);
 print("CXC : ".$cont);
 }
+/*
+SELECT A.SALDO_CXC
+    FROM ORSP_CL_ANTSAL_CLI_EX($RowQCliente->CLIENTE_ID, '".$Fechain."', '".$FechaFin."', 30, 'N', 'S') A;
+
+    select sum(A.SALDO_CXC)
+ from CLIENTES c
+LEFT join ORSP_CL_ANTSAL_CLI_EX(c.CLIENTE_ID, '2019-01-01', '2019-12-31', 30, 'N', 'S') A on (1=1);  
+*/$time_end = microtime(true);
+
+    //dividing with 60 will give the execution time in minutes otherwise seconds
+    $execution_time = ($time_end - $time_start);
+    
+    //execution time of the script
+    echo '<b>Actualización de cxc terminada:</b> '.(floor($execution_time*100)/100).' Segundos';
--- pull.bat
+++ pull.bat
@@ -1,2 +1,3 @@
+cd C:\xampp2\htdocs\data
 git pull origin master
 set /p a=Actualizado
